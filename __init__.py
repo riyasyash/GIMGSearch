a9e1ae8633a80864e9823971df76c45fbdd45df7
Riyas P <riyasp@sahajsoft.com>
2018-05-16 21:10:55 +0530

    first commit

---
+++ __init__.py
@@ -0,0 +1,26 @@
+from image_search import IMGSearch
+
+
+class GIMGSearch():
+    '''
+    Library to scrape Google images for the submitted key word and returns submitted number of image urls
+    '''
+
+    def __init__(self):
+        self.number_of_images_to_be_returned = 1  # by default setting the images to be returned as one.
+        self.key_word_to_be_searched = None
+
+    def search(self, key_word_to_be_searched=None,number_of_images_to_be_returned=None ):
+        '''
+
+        :param key_word_to_be_searched:
+        :param number_of_images_to_be_returned:
+        :return: image urls matching the submitted key_word
+        '''
+        if key_word_to_be_searched is None:
+            raise Exception('key word to search is not submitted')
+        self.key_word_to_be_searched = key_word_to_be_searched
+        if number_of_images_to_be_returned:
+            self.number_of_images_to_be_returned = number_of_images_to_be_returned
+
+        return IMGSearch().search(self.key_word_to_be_searched,self.number_of_images_to_be_returned)
---
+++ image_search.py
@@ -0,0 +1,6 @@
+class IMGSearch():
+    def __init__(self):
+        pass
+
+    def search(self,keyword,no_images):
+        pass
