04d1b6d8d7fe64cba0c30d2833faf8c605ed10e0
Riyas P <riyasp@sahajsoft.com>
2018-08-27 12:56:58 +0530

    including beatifulsoup and skelton code to return image urls

---
+++ config.py
@@ -0,0 +1,1 @@
+google_image_search_url = 'https://www.google.com/search?tbm=isch&q={search_key}'
--- image_search.py
+++ image_search.py
@@ -1,6 +1,20 @@
+from bs4 import BeautifulSoup
+
+from config import google_image_search_url
+import requests
+
 class IMGSearch():
     def __init__(self):
         pass
 
     def search(self,keyword,no_images):
-        pass
+        search_url = google_image_search_url.format(search_key = keyword)
+        params = {
+            'tbm':'isch',
+            'q':keyword
+        }
+        page = requests.get(search_url, params).content
+        soup = BeautifulSoup(page, 'html.parser')
+        image_tags = soup.find_all('img',limit=no_images)
+        image_urls = [image_tag.get('src') for image_tag in image_tags]
+        return image_urls
